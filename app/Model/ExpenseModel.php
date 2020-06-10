e5cceb38aacbca90006fe07aac5750358a7e60d4
ilkerguner <ilker.guner@asay.com.tr>
2020-06-10 17:06:40 +0300

    Expense List çekilirken ilgili expense' in projesi ve alt kategorilerinin de çekilmesi

--- app/Model/ExpenseModel.php
+++ app/Model/ExpenseModel.php
@@ -8,10 +8,29 @@ class ExpenseModel extends Model
 {
     protected $table = "Expense";
     protected $primaryKey = "id";
+    protected $appends = [
+        'Project',
+        'ProjectCategory',
+    ];
 
     public $timestamps = false;
     protected $fillable = [];
 
     protected $hidden = [];
     protected $casts = [];
+
+    public function getProjectAttribute()
+    {
+
+        $project = $this->hasOne(ProjectsModel::class,"id","project_id");
+        if ($project)
+        {
+            return $project->first();
+        }
+        else
+        {
+            return null;
+        }
+
+    }
 }
--- app/Model/ProjectsModel.php
+++ app/Model/ProjectsModel.php
@@ -8,10 +8,46 @@ class ProjectsModel extends Model
 {
     protected $table = "Projects";
     protected $primaryKey = "id";
+    protected $appends = [
+        'Manager',
+        'Category'
+    ];
 
     public $timestamps = false;
     protected $fillable = [];
 
     protected $hidden = [];
     protected $casts = [];
+
+    public function getManagerAttribute()
+    {
+
+        $manager = $this->hasOne(UserModel::class,"id","manager_id");
+        if ($manager)
+        {
+            return $manager->first();
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+
+    public function getCategoryAttribute()
+    {
+
+        $manager = $this->hasMany(ProjectCategoriesModel::class,"project_id","id");
+        if ($manager)
+        {
+            return $manager->get();
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+
+
 }
