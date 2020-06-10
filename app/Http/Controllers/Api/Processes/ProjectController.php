8ef4b509dd499764dcd4a1b9271fd1a93d92c709
ilkerguner <ilker.guner@asay.com.tr>
2020-06-10 18:32:05 +0300

    Harcamanın eklendiği formda, kullanıcının harcamayı sadece tanımlandığı proje ve alt projelere eklenmesi.

--- app/Http/Controllers/Api/Processes/ProjectController.php
+++ app/Http/Controllers/Api/Processes/ProjectController.php
@@ -17,13 +17,35 @@ class ProjectController extends ApiController
 
         $requestArray = $request->all();
 
-        $user = UserModel::find($requestArray['userId']);
+        $user = UserModel::find($request->userId);
+        $data = [];
 
-        $projectsOfUser = UserProjectsModel::where('user_id', $user->id)->get();
+        $projectsRelationsOfUser = UserProjectsModel::where('user_id', $user->id)->get();
+        $projectList = [];
+
+        if (count($projectsRelationsOfUser) < 1)
+        {
+            return response([
+                'status' => false,
+                'message' => 'Kullanıcı herhangi bir projede görev almıyor.'
+            ], 200);
+        }
+
+        foreach ($projectsRelationsOfUser as  $project)
+        {
+
+            $prj = ProjectsModel::find($project->project_id);
+            if($prj)
+                array_push($projectList,$prj);
+
+
+        }
+
+        $data['userProjectList'] = $projectList;
 
         return response([
             'status' => true,
-            'data' => $projectsOfUser
+            'data' => $data
         ], 200);
 
     }
