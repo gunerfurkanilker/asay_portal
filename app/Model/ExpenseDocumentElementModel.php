f1d5e3c94f6f6056e9ce76064a6f330cce900d50
serkan.erdinc <serkan.erdinc@asay.com.tr>
2020-07-02 12:05:30 +0300

    fixed

--- app/Model/ExpenseDocumentElementModel.php
+++ app/Model/ExpenseDocumentElementModel.php
@@ -13,4 +13,10 @@ class ExpenseDocumentElementModel extends Model
 
     protected $hidden = [];
     protected $casts = [];
+    protected $appends = ["expense_account_name"];
+
+    public function getExpenseAccountNameAttribute()
+    {
+        return ExpenseAccountCodesModel::whereRaw("CONCAT(account,'-',expense_type,'-',project,'-',project_category,'-',accounting_code)='".$this->attributes["expense_account"]."'")->first()->name;
+    }
 }
