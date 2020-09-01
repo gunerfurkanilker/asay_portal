df1854ef3e9b10d2df047efebb44c83d250a74b1
ilkerguner <ilker.guner@asay.com.tr>
2020-09-01 12:00:57 +0300

    Api Controller

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -30,7 +30,7 @@ class ApiController extends Controller
                 ->leftJoin("user","user.id","=","user_tokens.user_id")
                 ->where(["user_token"=>$token])->first();
             $request->userId        = $user->id;
-            //$request->EmployeeID    = $user->EmployeeID;
+            $request->Employee      = $user->EmployeeID;
             return $next($request);
         });
     }
